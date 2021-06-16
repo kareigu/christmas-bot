df91a9c6c3d047fbec0defcc4e62fcec70d79b08
mxr <eetu.kivelae@gmail.com>
2021-06-16 15:31:09 +0300

    Feature: Implemented bot loop Bot now starts up and connects to Discord's servers.

--- src/main.go
+++ src/main.go
@@ -3,17 +3,37 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	env_err := godotenv.Load()
-	if env_err != nil {
+	err := godotenv.Load()
+	if err != nil {
 		log.Fatal("Couldn't load env file")
 	}
 
 	bot_token := os.Getenv("TOKEN")
 
-	log.Println(bot_token)
+	client, err := discordgo.New("Bot " + bot_token)
+	if err != nil {
+		log.Fatal("Couldn't create bot client")
+	}
+
+	err = client.Open()
+	if err != nil {
+		log.Fatal("Couldn't start bot client")
+	}
+
+	client.Identify.Intents = discordgo.IntentsGuildMessages
+
+	log.Println("Running...")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	client.Close()
 }
